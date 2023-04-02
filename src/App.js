50d141e3434cf70491a396264db14c7d69d3722d
debcrisjol <debcrisjol@gmail.com>
2023-04-02 19:15:25 +0100

    card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Navbar from './components/navbar';
+import Card from './components/card';
+
+
 function App() {
   return (
     <>
@@ -6,6 +9,10 @@ function App() {
    <div className="container">
    <h1>Cosa desideri ordinare?</h1>
    <hr/>
+   <div className="row">
+    <Card />
+   </div>
+
    </div>
    </>
   );
