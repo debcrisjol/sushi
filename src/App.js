dbf337d82d5c3aad351856b5995bbb72d65b06d7
debcrisjol <debcrisjol@gmail.com>
2023-04-02 21:03:26 +0100

    increment

--- src/App.js
+++ src/App.js
@@ -12,16 +12,23 @@ import shrimp from "./images/shrimp.png";
 class App extends Component {
   state = {
     cards: [
-      { id: 0, nome: "California", prezzo: 1.99, immagine: california },
-      { id: 1, nome: "Dragon", prezzo: 3.99, immagine: dragon },
-      { id: 2, nome: "Dynamite", prezzo: 4.99, immagine: dynamite },
-      { id: 3, nome: "Philadelphia", prezzo: 2.99, immagine: philadelphia },
-      { id: 4, nome: "Rainbow", prezzo: 1.09, immagine: rainbow },
-      { id: 5, nome: "Shrimp", prezzo: 3.79, immagine: shrimp },
+      { id: 0, nome: "California", prezzo: 1.99, immagine: california, quantita:0 },
+      { id: 1, nome: "Dragon", prezzo: 3.99, immagine: dragon,quantita:0 },
+      { id: 2, nome: "Dynamite", prezzo: 4.99, immagine: dynamite,quantita:0 },
+      { id: 3, nome: "Philadelphia", prezzo: 2.99, immagine: philadelphia,quantita:0 },
+      { id: 4, nome: "Rainbow", prezzo: 1.09, immagine: rainbow, quantita:0 },
+      { id: 5, nome: "Shrimp", prezzo: 3.79, immagine: shrimp ,quantita:0 },
     ],
   };
   handleDelete = cardId =>{
     const cards =this.state.cards.filter(card => card.id !== cardId);
+    this.setState({cards});
+  }
+  handleIncrement = card =>{
+    const cards =[...this.state.cards];
+    const id= cards.indexOf(card);
+    cards[id]={...card};
+    cards[id].quantita++;
     this.setState({cards});
   }
   render() {
@@ -36,6 +43,7 @@ class App extends Component {
               <Card
                 key={card.id}
                 onDelete={this.handleDelete}
+                onIncrement={this.handleIncrement}
                card={card}
               />
             ))}
--- src/components/card.js
+++ src/components/card.js
@@ -5,6 +5,8 @@ class Card extends Component{
     return(
         <div className="col">
 <div className="card" style={{width: '18rem', textAlign:'center'}}>
+<button onClick={()=>this.props.onIncrement(this.props.card)} className="btn btn-primary">Aggiungi<span className='badge ms-3 bg-light text-success'>{this.props.card.quantita}</span></button>
+
   <img src={this.props.card.immagine} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">{this.props.card.nome} Roll</h5>
