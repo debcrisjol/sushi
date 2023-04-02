968d7ab88ec968ef7463b503b8cbbdff785e2fda
debcrisjol <debcrisjol@gmail.com>
2023-04-02 20:09:31 +0100

    elimina card

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import React,{Component} from'react';
+import React, { Component } from "react";
 
-import Navbar from './components/navbar';
-import Card from './components/card';
-import california from './images/california.png';
-import dragon from './images/dragon.png';
-import dynamite from './images/dynamite.png';
-import philadelphia from './images/philadelphia.png';
-import rainbow from './images/rainbow.png';
-import shrimp from './images/shrimp.png';
-
+import Navbar from "./components/navbar";
+import Card from "./components/card";
+import california from "./images/california.png";
+import dragon from "./images/dragon.png";
+import dynamite from "./images/dynamite.png";
+import philadelphia from "./images/philadelphia.png";
+import rainbow from "./images/rainbow.png";
+import shrimp from "./images/shrimp.png";
 
 class App extends Component {
-  state = {cards:[
-    {id:0, nome: "California",prezzo: 1.99, immagine: california},
-    {id:1, nome: "Dragon",prezzo: 3.99, immagine: dragon},
-    {id:2, nome: "Dynamite",prezzo: 4.99, immagine: dynamite},
-    {id:3, nome: "Philadelphia",prezzo: 2.99, immagine: philadelphia},
-    {id:4, nome: "Rainbow",prezzo: 1.09, immagine: rainbow},
-    {id:5, nome: "Shrimp",prezzo: 3.79, immagine: shrimp},
-  ]}
-  render(){
-  return (
-    <>
-   <Navbar />
-   <div className="container">
-   <h1>Cosa desideri ordinare?</h1>
-   <hr/>
-   <div className="row">
-   {this.state.cards.map(card => (
-   <Card 
-   key={card.id}
-   nome={card.nome}
-   prezzo={card.prezzo}
-   immagine={card.immagine} />
-   ))}
-   </div>
-
-   </div>
-   </>
-  );
-}
+  state = {
+    cards: [
+      { id: 0, nome: "California", prezzo: 1.99, immagine: california },
+      { id: 1, nome: "Dragon", prezzo: 3.99, immagine: dragon },
+      { id: 2, nome: "Dynamite", prezzo: 4.99, immagine: dynamite },
+      { id: 3, nome: "Philadelphia", prezzo: 2.99, immagine: philadelphia },
+      { id: 4, nome: "Rainbow", prezzo: 1.09, immagine: rainbow },
+      { id: 5, nome: "Shrimp", prezzo: 3.79, immagine: shrimp },
+    ],
+  };
+  handleDelete = cardId =>{
+    const cards =this.state.cards.filter(card => card.id !== cardId);
+    this.setState({cards});
+  }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+          <h1>Cosa desideri ordinare?</h1>
+          <hr />
+          <div className="row">
+            {this.state.cards.map((card) => (
+              <Card
+                key={card.id}
+                onDelete={this.handleDelete}
+               card={card}
+              />
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 export default App;
