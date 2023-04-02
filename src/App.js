6ea1ebd2537782b2a30c0a80c5c417855bfff224
debcrisjol <debcrisjol@gmail.com>
2023-04-02 19:40:18 +0100

    ciclare dei card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React,{Component} from'react';
+
 import Navbar from './components/navbar';
 import Card from './components/card';
+import california from './images/california.png';
+import dragon from './images/dragon.png';
+import dynamite from './images/dynamite.png';
+import philadelphia from './images/philadelphia.png';
+import rainbow from './images/rainbow.png';
+import shrimp from './images/shrimp.png';
 
 
-function App() {
+class App extends Component {
+  state = {cards:[
+    {id:0, nome: "California",prezzo: 1.99, immagine: california},
+    {id:1, nome: "Dragon",prezzo: 3.99, immagine: dragon},
+    {id:2, nome: "Dynamite",prezzo: 4.99, immagine: dynamite},
+    {id:3, nome: "Philadelphia",prezzo: 2.99, immagine: philadelphia},
+    {id:4, nome: "Rainbow",prezzo: 1.09, immagine: rainbow},
+    {id:5, nome: "Shrimp",prezzo: 3.79, immagine: shrimp},
+  ]}
+  render(){
   return (
     <>
    <Navbar />
@@ -10,12 +27,18 @@ function App() {
    <h1>Cosa desideri ordinare?</h1>
    <hr/>
    <div className="row">
-    <Card />
+   {this.state.cards.map(card => (
+   <Card 
+   key={card.id}
+   nome={card.nome}
+   prezzo={card.prezzo}
+   immagine={card.immagine} />
+   ))}
    </div>
 
    </div>
    </>
   );
 }
-
+}
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,19 @@
-import california from'../images/california.png';
-function Card(){
+import React, { Component } from 'react';
+
+class Card extends Component{
+    render(){
     return(
         <div className="col">
 <div className="card" style={{width: '18rem', textAlign:'center'}}>
-  <img src={california} className="card-img-top" alt="..." />
+  <img src={this.props.immagine} className="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">California Roll</h5>
-    <p className="card-text"> £1.99</p>
+    <h5 className="card-title">{this.props.nome} Roll</h5>
+    <p className="card-text">£{this.props.prezzo}</p>
     <button className="btn btn-outline-danger">Elimina</button>
   </div>
 </div>
         </div>
     );
 
-}
+}}
 export default Card;
